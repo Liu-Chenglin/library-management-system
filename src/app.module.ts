f453457e2c739e128b8b79449adbe9d98bb51db6
Liu Chenglin <liu_cheng_lin@126.com>
2023-02-17 09:44:06 +0800

    [#4] chore: configure connection to postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { BooksModule } from './models/books/books.module';
+import {Module} from '@nestjs/common';
+import {BooksModule} from './models/books/books.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {BookEntity} from "./models/books/entities/book.entity";
 
 @Module({
-  imports: [BooksModule],
-  controllers: [],
-  providers: [],
+    imports: [BooksModule, TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'library_management_system',
+        entities: [BookEntity],
+        synchronize: true,
+    }),],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
