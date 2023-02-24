32c4683cb67262a86a14cae1a9cc47c7055bcfcc
Liu Chenglin <liu_cheng_lin@126.com>
2023-02-24 15:33:07 +0800

    [#3] feat: can throw exception when creating invalid student

--- src/models/students/students.controller.spec.ts
+++ src/models/students/students.controller.spec.ts
@@ -3,10 +3,14 @@ import {StudentsController} from './students.controller';
 import {StudentsService} from './students.service';
 import {CreateStudentDto} from "./dto/create-student.dto";
 import {Student} from "./student";
+import * as request from "supertest";
+import {HttpStatus, INestApplication} from "@nestjs/common";
+import {AppModule} from "../../app.module";
 
 describe('StudentsController', () => {
     let studentsController: StudentsController;
     let studentsService: StudentsService;
+    let app: INestApplication;
 
     const mockStudentsService = {
         create: jest.fn(),
@@ -14,6 +18,7 @@ describe('StudentsController', () => {
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
             controllers: [StudentsController],
             providers: [
                 {
@@ -22,6 +27,8 @@ describe('StudentsController', () => {
                 },
             ],
         }).compile();
+        app = module.createNestApplication();
+        await app.init();
 
         studentsController = module.get<StudentsController>(StudentsController);
         studentsService = module.get<StudentsService>(StudentsService);
@@ -32,7 +39,7 @@ describe('StudentsController', () => {
             name: 'John Doe',
             grade: 1,
             type: 'Undergraduate',
-            phone: '12345678901',
+            phone: '13912345678',
             email: 'johndoe@example.com',
         }
 
@@ -49,6 +56,29 @@ describe('StudentsController', () => {
 
             expect(studentsService.create).toHaveBeenCalledWith(createStudentDto);
             expect(result).toEqual(createdStudent);
+        });
+
+        it('should return BAD REQUEST when given student has invalid phone number', async () => {
+            const {phone, ...invalidCreateStudentDto} = createStudentDto;
+            const incorrectCreateStudentDto = {phone: '1391234567', ...invalidCreateStudentDto};
+
+            const response = await request(app.getHttpServer())
+                .post('/students')
+                .send(incorrectCreateStudentDto)
+                .expect(HttpStatus.BAD_REQUEST);
+
+            expect(response.body.message.message).toContain('phone must be a phone number');
+        });
+
+        it('should return BAD REQUEST when given student missing email', async () => {
+            const {email, ...invalidCreateStudentDto} = createStudentDto;
+
+            const response = await request(app.getHttpServer())
+                .post('/students')
+                .send(invalidCreateStudentDto)
+                .expect(HttpStatus.BAD_REQUEST);
+            
+            expect(response.body.message.message).toContain('email must be an email');
         });
     });
 });
