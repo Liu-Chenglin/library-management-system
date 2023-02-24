fc4009d60fe7444870ef0e87a4ab15f70176a9fe
Liu Chenglin <liu_cheng_lin@126.com>
2023-02-24 15:45:07 +0800

    [#3] refactor: extract createStudentEntity and findStudentTypeOrThrow

--- src/models/students/students.service.ts
+++ src/models/students/students.service.ts
@@ -7,6 +7,7 @@ import {plainToClass} from "class-transformer";
 import {StudentEntity} from "./entities/student.entity";
 import {StudentsMapper} from "../../utils/mappers/students/students.mapper";
 import {StudentTypeMapper} from "../../utils/mappers/students/student-type.mapper";
+import {StudentTypeEntity} from "./entities/student-type.entity";
 
 @Injectable()
 export class StudentsService {
@@ -16,20 +17,30 @@ export class StudentsService {
     }
 
     async create(createStudentDto: CreateStudentDto): Promise<Student> {
+        const studentTypeEntity = await this.findStudentTypeOrThrow(createStudentDto);
+
+        const createdStudentEntity = this.createStudentEntity(createStudentDto, studentTypeEntity);
+        const savedStudentEntity = await this.studentsRepository.save(createdStudentEntity);
+
+        const student = StudentsMapper.toModel(savedStudentEntity);
+        student.type = StudentTypeMapper.toModel(studentTypeEntity);
+        return student;
+    }
+
+    private createStudentEntity(createStudentDto: CreateStudentDto, studentTypeEntity: StudentTypeEntity) {
+        const createdStudentEntity = plainToClass(StudentEntity, createStudentDto);
+        createdStudentEntity.availableQuota = studentTypeEntity.quota;
+        createdStudentEntity.type = studentTypeEntity;
+        return createdStudentEntity;
+    }
+
+    private async findStudentTypeOrThrow(createStudentDto: CreateStudentDto) {
         const studentTypeEntity = await this.studentTypeRepository.findByType(createStudentDto.type);
 
         if (!studentTypeEntity) {
             throw new HttpException("Student Type Not Found: " + createStudentDto.type, HttpStatus.NOT_FOUND);
         }
 
-        const createdStudentEntity = plainToClass(StudentEntity, createStudentDto);
-        createdStudentEntity.availableQuota = studentTypeEntity.quota;
-        createdStudentEntity.type = studentTypeEntity;
-
-        const savedStudentEntity = await this.studentsRepository.save(createdStudentEntity);
-
-        const student = StudentsMapper.toModel(savedStudentEntity);
-        student.type = StudentTypeMapper.toModel(studentTypeEntity);
-        return student;
+        return studentTypeEntity;
     }
 }
