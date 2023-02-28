5ff9ee38c747699a3adcaf8df8bff2d487392a95
Liu Chenglin <liu_cheng_lin@126.com>
2023-02-28 11:00:40 +0800

    [#1] refactor: extract updateStudentAndBookStatus and validateBorrowingOrThrow

--- src/services/book-borrowing/book-borrowing.service.ts
+++ src/services/book-borrowing/book-borrowing.service.ts
@@ -3,6 +3,8 @@ import {BookBorrowingRepository} from "./entities/book-borrowing.repository";
 import {StudentsService} from "../../models/students/students.service";
 import {BooksService} from "../../models/books/books.service";
 import {BookStatus} from "../../common/constants/books.constant";
+import {BookEntity} from "../../models/books/entities/book.entity";
+import {StudentEntity} from "../../models/students/entities/student.entity";
 
 @Injectable()
 export class BookBorrowingService {
@@ -14,20 +16,11 @@ export class BookBorrowingService {
 
     async borrow(studentId: number, bookId: number) {
         const bookEntity = await this.booksService.findOneByIdOrThrow(bookId);
-
-        if (BookStatus.AVAILABLE !== bookEntity.status) {
-            throw new HttpException("Book is not available", HttpStatus.BAD_REQUEST);
-        }
-
         const studentEntity = await this.studentsService.findOneByIdOrThrow(studentId);
-        if (studentEntity.availableQuota <= 0) {
-            throw new HttpException("Student don't have available quota", HttpStatus.BAD_REQUEST);
-        }
 
-        bookEntity.status = BookStatus.BORROWED;
-        await this.booksService.save(bookEntity);
-        studentEntity.availableQuota -= 1;
-        await this.studentsService.save(studentEntity);
+        this.validateBorrowingOrThrow(bookEntity, studentEntity);
+
+        await this.updateStudentAndBookStatus(bookEntity, studentEntity);
 
         const dueDate = new Date();
         dueDate.setDate(dueDate.getDate() + studentEntity.type.maxLoanPeriod);
@@ -39,5 +32,22 @@ export class BookBorrowingService {
         });
 
         await this.bookBorrowingRepository.save(createdBookBorrowingEntity);
+    }
+
+    private async updateStudentAndBookStatus(bookEntity: BookEntity, studentEntity: StudentEntity) {
+        bookEntity.status = BookStatus.BORROWED;
+        await this.booksService.save(bookEntity);
+        studentEntity.availableQuota -= 1;
+        await this.studentsService.save(studentEntity);
+    }
+
+    private validateBorrowingOrThrow(bookEntity: BookEntity, studentEntity: StudentEntity) {
+        if (BookStatus.AVAILABLE !== bookEntity.status) {
+            throw new HttpException("Book is not available", HttpStatus.BAD_REQUEST);
+
+        }
+        if (studentEntity.availableQuota <= 0) {
+            throw new HttpException("Student don't have available quota", HttpStatus.BAD_REQUEST);
+        }
     }
 }
